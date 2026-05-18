9860a905e73ba6eda33dc6e34fc3640863f78cc3
christian bechi <christian.bechi@adsr.com.br>
2026-05-18 03:31:20 -0300

    ajus

--- src/sections/ExploreListingsSection.tsx
+++ src/sections/ExploreListingsSection.tsx
@@ -63,6 +63,10 @@ export default function ExploreListingsSection() {
     }
   }, [activeTab, favoriteListings, isFavorite, listings])
 
+  if (!listings.length) {
+    return null
+  }
+
   return (
     <Box>
       <HStack
